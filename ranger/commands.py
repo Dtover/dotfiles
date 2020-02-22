e23ce7f17492a05d223778c313934a64afc31e57
Dtover <1530244379@qq.com>
2020-02-22 10:09:33 +0800

    --

--- ranger/commands.py
+++ ranger/commands.py
@@ -14,6 +14,7 @@ import os
 
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
+from ranger.core.loader import CommandLoader
 
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
@@ -61,6 +62,7 @@ class my_edit(Command):
         # content of the current directory.
         return self._tab_directory_content()
 
+
 class fzf_select(Command):
     """
     :fzf_select
@@ -76,13 +78,17 @@ class fzf_select(Command):
         import os.path
         if self.quantifier:
             # match only directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+
         else:
             # match files and directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+
+        fzf = self.fm.execute_command(command,
+                                      universal_newlines=True,
+                                      stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
@@ -91,3 +97,95 @@ class fzf_select(Command):
             else:
                 self.fm.select_file(fzf_file)
 
+
+class extract(Command):
+    """:extract <paths>
+    Extract archives using 7z
+    """
+    def execute(self):
+        import os
+        fail = []
+        for i in self.fm.thistab.get_selection():
+            ExtractProg = '7z x'
+            if i.path.endswith('.zip'):
+                # zip encoding issue
+                ExtractProg = 'unzip -O gbk'
+            elif i.path.endswith('.tar.gz'):
+                ExtractProg = 'tar xvf'
+            elif i.path.endswith('.tar.xz'):
+                ExtractProg = 'tar xJvf'
+            elif i.path.endswith('.tar.bz2'):
+                ExtractProg = 'tar xjvf'
+            if os.system('{0} "{1}"'.format(ExtractProg, i.path)):
+                fail.append(i.path)
+        if len(fail) > 0:
+            self.fm.notify("Fail to extract: {0}".format(' '.join(fail)),
+                           duration=10,
+                           bad=True)
+        self.fm.redraw_window()
+
+
+class mkcd(Command):
+    """
+    :mkcd <dirname>
+    Creates a directory with the name <dirname> and enters it.
+    """
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        import re
+
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+
+            match = re.search('^/|^~[^/]*/', dirname)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0):]
+
+            for m in re.finditer('[^/]+', dirname):
+                s = m.group(0)
+                if s == '..' or (s.startswith('.')
+                                 and not self.fm.settings['show_hidden']):
+                    self.fm.cd(s)
+                else:
+                    ## We force ranger to load content before calling `scout`.
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
+
+
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
+
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+    def tab(self, tabnum):
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return [
+            'compress ' + os.path.basename(self.fm.thisdir.path) + ext
+            for ext in extension
+        ]
